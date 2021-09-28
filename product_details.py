8755c27add2cc6ca45f81266e05a44a811d67f38
Pavel Pavlov <gsxr_1000@abv.bg>
2021-09-28 20:17:50 +0300

    update

--- product_details.py
+++ product_details.py
@@ -7,74 +7,90 @@ base_url = "https://www.domko.com"
 
 links = get_urls()
 
-for link_list in links:
-    for link in link_list:
-        product_url = base_url + link
+for link in links:
+    product_url = base_url + link
 
-        result = requests.get(product_url)
-        doc = BeautifulSoup(result.text, "html.parser")
+    result = requests.get(product_url)
+    doc = BeautifulSoup(result.text, "html.parser")
 
-        product_info = doc.find("div", id="product-information")
+    image_url = doc.find("div", id="product-details-image").find("img").get("src")
+    image = base_url + str(image_url)
 
-        image_url = doc.find("div", id="product-details-image").find("img").get("src")
-        image = base_url + str(image_url)
+    category = doc.find("div", id="breadcrumbs").find_all("a")[-2].text
 
-        category = doc.find("div", id="breadcrumbs").find_all("a")[-2].text
+    product_info = doc.find("div", id="product-information")
 
-        title = product_info.h2.text
-        price = product_info.h3.span.text
-        product_number = product_info.find("p", class_="pr-code").span.text
-        collection = product_info.find("p", class_="collection").a.text
-        all_a = product_info.find_all("p", class_="collection")
-        brand = ""
+    title = product_info.h2.text
+    price = product_info.h3.span.text
+    product_number = product_info.find("p", class_="pr-code").span.text
+    collection = product_info.find("p", class_="collection").a.text
 
-        for a in all_a:
-            if "Марка" in a.text:
-                brand = a.text.split(":")[-1].strip(" ")
+    all_a = product_info.find_all("p", class_="collection")
+    brand = ""
 
-        product_form = product_info.find("form")
+    for a in all_a:
+        if "Марка" in a.text:
+            brand = a.text.split(":")[-1].strip(" ")
 
+    if brand == "":
+        category = doc.find("div", id="breadcrumbs").find_all("a")[-1].text
+
+    sizes = ""
+    sustav = ""
+    colors = ""
+    vid = ""
+    tip = ""
+    prednaznachenie = ""
+
+    product_form = product_info.find("form")
+
+    def get_options(category, options):
+        for i in options[1:]:
+            category += (i.text).join(" ,").strip()
+        return category
+
+
+    if product_form.find("select", id="razmer"):
         size_options = list(product_form.find("select", id="razmer").children)
-        sustav_options = list(product_form.find("select", id="sastav").children)
-        color_options = list(product_form.find("select", id="tsvyat").children)
-        vid_options = list(product_form.find("select", id="vid").children)
-        tip_options = list(product_form.find("select", id="tip").children)
-        prednaznachenie_options = list(product_form.find("select", id="prednaznachenie").children)
-
-        sizes = ""
-        sustav = ""
-        colors = ""
-        vid = ""
-        tip = ""
-        prednaznachenie = ""
-
-
-        def get_options(category, options):
-            for i in options[1:]:
-                category += (i.text).join(" ,").strip()
-            return category
-
-
         sizes = get_options(sizes, size_options)
+
+    if product_form.find("select", id="sastav"):
+        sustav_options = list(product_form.find("select", id="sastav").children)
         sustav = get_options(sustav, sustav_options)
+
+    if product_form.find("select", id="tsvyat"):
+        color_options = list(product_form.find("select", id="tsvyat").children)
         colors = get_options(colors, color_options)
+
+    if product_form.find("select", id="vid"):
+        vid_options = list(product_form.find("select", id="vid").children)
         vid = get_options(vid, vid_options)
+
+    if product_form.find("select", id="tip"):
+        tip_options = list(product_form.find("select", id="tip").children)
         tip = get_options(tip, tip_options)
+
+    if product_form.find("select", id="prednaznachenie"):
+        prednaznachenie_options = list(product_form.find("select", id="prednaznachenie").children)
         prednaznachenie = get_options(prednaznachenie, prednaznachenie_options)
 
-        opisanie = doc.find("div", id="product-text").text.strip()
+    opisanie = doc.find("div", id="product-text").text.strip()
 
-        print(image)
-        print(category)
-        print(title)
-        print(price)
-        print(product_number)
-        print(collection)
-        print(brand)
-        print(sizes)
-        print(sustav)
-        print(colors)
-        print(vid)
-        print(tip)
-        print(prednaznachenie)
-        print(opisanie)
+    print(image)
+    # print(category)
+    print(title)
+    print(price)
+    print(product_number)
+    print(collection)
+    print(brand)
+    print(sizes)
+    print(sustav)
+    print(colors)
+    print(vid)
+    print(tip)
+    print(prednaznachenie)
+    print(opisanie)
+
+# взима само основната снимка
+# не взима намалени цени
+# може да обърка категорията!!!
--- product_search.py
+++ product_search.py
@@ -1,23 +1,36 @@
 from bs4 import BeautifulSoup
 import requests
 
-product_search = input("Какво търсиш?")
+print("Какво търсиш?")
+product_search = input()
 
 
 def get_urls():
     url = f"https://www.domko.com/search/page=1/{product_search}/"
     result = requests.get(url)
     doc = BeautifulSoup(result.text, "html.parser")
-
-    pages_count = int(doc.find("div", id="pagination").find_all("a")[-2].text)
+    # category = doc.find("div", id="breadcrumbs").find_all("li")[-1].text
     links = []
 
-    for page in range(1, pages_count + 1):
-        url = f"https://www.domko.com/search/page={page}/{product_search}/"
-        result = requests.get(url)
-        doc = BeautifulSoup(result.text, "html.parser")
+    if doc.find("div", id="pagination").a:
+        pages_count = int(doc.find("div", id="pagination").find_all("a")[-2].text)
 
+        for page in range(1, pages_count + 1):
+            url = f"https://www.domko.com/search/page={page}/{product_search}/"
+            result = requests.get(url)
+            doc = BeautifulSoup(result.text, "html.parser")
+
+            div = doc.find(id="product-listing")
+
+            for a in div.find_all('a', href=True):
+                links.append(a.get('href'))
+
+        return links
+
+    else:
         div = doc.find(id="product-listing")
-        links.append([a.get('href') for a in div.find_all('a', href=True)])
+
+        for a in div.find_all('a', href=True):
+            links.append(a.get('href'))
 
     return links
